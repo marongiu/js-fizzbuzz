72e5d56c53a892632d196ce288d4c02a0657b277
Andrea <andreamarongiu05@gmail.com>
2021-03-05 15:31:34 +0100

    Termine esercizio

--- main.js
+++ main.js
@@ -2,9 +2,16 @@
 
 for (var i = 1; i <= 100; i++) {
   // Devo determinare se i numeri sono multipli di 3 e 5 o entrambi
-  if (i % 3 == 0) {
-    console.log(i, "Fizz");
+  // Determino se sia multiplo di 5 e di 3
+  if ((i % 3 == 0) && (i % 5 == 0)) {
+    console.log("Fizz Buzz");
+    // Determino se è multiplo di 5
   } else if (i % 5 == 0) {
-    console.log(i, "Buzz");
+    console.log("Buzz");
+    // Determino se è multiplo di 3
+  } else if (i % 3 == 0){
+    console.log("Fizz");
+  } else {
+    console.log(i);
   }
 }
