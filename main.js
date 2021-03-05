cffbbbb5a8dcb870b97e4596a070f0b3683eb328
Andrea <andreamarongiu05@gmail.com>
2021-03-05 15:12:28 +0100

    Determino se sono multipli di 3 o 5

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 // Creo un ciclo che mi stampa 100 numeri
 
 for (var i = 1; i <= 100; i++) {
-  console.log(i);
+  // Devo determinare se i numeri sono multipli di 3 e 5 o entrambi
+  if (i % 3 == 0) {
+    console.log(i, "Fizz");
+  } else if (i % 5 == 0) {
+    console.log(i, "Buzz");
+  }
 }
